11a6150b913fbaa2f6327bfd5c7768ab018c95d0
Gabriel Martins <martinsgabriel1956@gmail.com>
2022-05-29 21:55:52 -0300

    feat: third day finished

---
+++ public/scripts/scrollreveal.js
@@ -0,0 +1,15 @@
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true,
+})
+
+scrollReveal.reveal(
+  `#home .img-container, #home .text-container,
+  #about .img-container, #about .text-container,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text-container, #contact .links
+`, 
+{ interval: 100 })
